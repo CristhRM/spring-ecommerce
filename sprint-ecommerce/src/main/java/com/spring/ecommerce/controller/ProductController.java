fb49cfffdccfd44dbf0da7a50498e85bef9aad8c
Cristhian Andres Rios <andresrios171@gmail.com>
2022-12-19 17:37:03 -0500

    crud upload images

--- sprint-ecommerce/src/main/java/com/spring/ecommerce/controller/ProductController.java
+++ sprint-ecommerce/src/main/java/com/spring/ecommerce/controller/ProductController.java
@@ -3,13 +3,16 @@ package com.spring.ecommerce.controller;
 import com.spring.ecommerce.model.Product;
 import com.spring.ecommerce.model.User;
 import com.spring.ecommerce.service.ProductService;
+import com.spring.ecommerce.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +22,9 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private UploadFileService uploadFileService;
 
     @GetMapping
     public String show(Model model) {
@@ -32,10 +38,15 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String save(Product product) {
+    public String save(Product product, @RequestParam("img") MultipartFile multipartFile) throws IOException {
         LOGGER.info("This its Object of product {} ", product);
         User user = new User(1, "", "", "", "", "", "", "");
         product.setUser(user);
+
+        if (product.getId() == null) {
+            product.setImage(uploadFileService.saveImage(multipartFile));
+        }
+
         productService.save(product);
         return "redirect:/products";
     }
@@ -50,14 +61,29 @@ public class ProductController {
     }
 
     @PostMapping("/update")
-    public String update(Product product) {
-        product.setUser(new User(1, "", "", "", "", "", "", ""));
+    public String update(Product product, @RequestParam("img") MultipartFile multipartFile) throws IOException {
+        Product productFill;
+        productFill = productService.get(product.getId()).get();
+        if (multipartFile.isEmpty()) {
+            product.setImage(productFill.getImage());
+        } else {
+            if (!productFill.getImage().equals("default.jpg")) {
+                uploadFileService.delete(productFill.getImage());
+            }
+            product.setImage(uploadFileService.saveImage(multipartFile));
+
+        }
+        product.setUser(productFill.getUser());
         productService.update(product);
         return "redirect:/products";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Integer id) {
+        Product product = productService.get(id).get();
+        if (!product.getImage().equals("default.jpg")) {
+            uploadFileService.delete(product.getImage());
+        }
         productService.delete(id);
         return "redirect:/products";
     }
